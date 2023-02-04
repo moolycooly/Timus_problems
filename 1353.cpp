bbb6e704c0b8b120b5b9d0b4d10012f44ce80900
Sergey <118826947+moolycooly@users.noreply.github.com>
2023-02-05 01:30:14 +0300

    Update 1353.cpp

--- 1353.cpp
+++ 1353.cpp
@@ -1,50 +1,7 @@
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
-const int mod = 1e9+7;
-const int mx = 1e7;
-const int INF = 1e18;
-int fact[mx];
-//Template Functions..
-int binpow(int a,int n)
-{
 
-    if(!n) return 1;
-    if(n==1) return a;
-    if(n%2==1) return a*binpow(a,n-1);
-    else
-    {
-        int t = binpow(a,n/2);
-        return t*t;
-    }
-}
-int inverse(int x)
-{
-    return binpow(x,mod-2);
-}
-int div_mod(int a, int b)
-{
-    return a*inverse(b)%mod;
-}
-int cnk(int n, int k)
-{
-    if(n<k) return 0;
-    return div_mod(fact[n],fact[k]*fact[n-k]%mod);
-}
-
-int gcd(int a, int b)
-{
-    if(!b)return a;
-    return gcd(b,a%b);
-}
-int sum(int l, int r,vector<int>&a)
-{
-    if(l > r) return 0;
-    if(!l) return a[r];
-    return a[r]-a[l-1];
-
-}
-//
 main()
 {
     //Solution
